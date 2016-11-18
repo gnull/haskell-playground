fbef5c459f40d233591bf1311830ac5e5c753d87
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2016-11-18 16:31:44 +0300

    nano-parsec: Add an instance for Applicative

--- nano-parsec/main.hs
+++ nano-parsec/main.hs
@@ -1,4 +1,6 @@
 -- Example from tutorial at dev.stephendiehl.com
+import Data.Functor (Functor)
+import Control.Applicative (Applicative, pure, (<*>))
 
 newtype Parser a = Parser { parse :: String -> [(a, String)] }
 
@@ -19,3 +21,14 @@ unit a = Parser $ \s -> [(a, s)]
 bind :: Parser a -> (a -> Parser b) -> Parser b
 bind p f = Parser $
   \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \s -> [(f a, s') | (a, s') <- p s]
+
+instance Applicative Parser where
+  pure = unit
+  (Parser p1) <*> (Parser p2) = Parser $ \s ->
+    do
+      (f, s1) <- p1 s
+      (a, s2) <- p2 s1
+      return (f a, s2)
