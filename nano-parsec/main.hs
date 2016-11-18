d1776d40944b61fea066276ed6d7b8de2cbbbad4
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2016-11-18 17:03:13 +0300

    nano-parsec: Add an instance for Monad
    Conflicts:
    	nano-parsec/main.hs

--- nano-parsec/main.hs
+++ nano-parsec/main.hs
@@ -1,6 +1,7 @@
 -- Example from tutorial at dev.stephendiehl.com
 import Data.Functor (Functor)
 import Control.Applicative (Applicative, pure, (<*>))
+import Control.Monad (Monad, (>>=), return)
 
 newtype Parser a = Parser { parse :: String -> [(a, String)] }
 
@@ -15,18 +16,16 @@ item = Parser $ \s -> case s of
   [] -> []
   x:xs -> [(x, xs)]
 
-unit :: a -> Parser a
-unit a = Parser $ \s -> [(a, s)]
-
-bind :: Parser a -> (a -> Parser b) -> Parser b
-bind p f = Parser $
-  \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
+instance Monad Parser where
+  return a = Parser $ \s -> [(a, s)]
+  p >>= f = Parser $
+    \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
 
 instance Functor Parser where
   fmap f (Parser p) = Parser $ \s -> [(f a, s') | (a, s') <- p s]
 
 instance Applicative Parser where
-  pure = unit
+  pure = return
   (Parser p1) <*> (Parser p2) = Parser $ \s ->
     do
       (f, s1) <- p1 s
